eb802bcdaa6286456a6068ba55b6ff445c919c6b
eanswer <jiex@mit.edu>
2019-10-09 13:25:44 -0400

    assignment 3

---
+++ assignment3/src/cotangent_laplacian.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Eigen/Sparse>
+#include <igl/edge_lengths.h>
+#include <igl/face_areas.h>
+#include <igl/dihedral_angles.h>
+#include <igl/volume.h>
+#include "dihedral_sine.hpp"
+
+// TODO: HW3
+// Assignment 3, Part 3.2.
+/* Implement your code here. */
+// Implement the function to compute the cotangent laplacian matrix L 
+// V is the vertex matrix of shape (n, 3), each row is the position of a vertex in the mesh
+// F is the element index matrix of shape (m, 4), each row is the vertex indices of a tetrahedron
+// L is the output cotangent laplacian matrix of shape (n, n), and it's a sparse matrix.
+// Hints:
+	// 1. For each tetrahedron, loop over each of its edge,
+	//    consider which part of the L matrix this edge in this tetrahedron contributes to
+	// 2. compute the cos and sin of the dihedral angle by the law of diehedral angles http://mathworld.wolfram.com/Tetrahedron.html
+	//	  specifically, compute the sin and cos of dihedral angles from the edge lengths, face areas and tet volume
+	// 3. build the triplets <row, col, value> in IJV
+void cotangent_laplacian(
+	const Eigen::MatrixXd& V, 
+	const Eigen::MatrixXi& F, 
+	Eigen::SparseMatrix<double>& L) 
+{
+	L.resize(V.rows(), V.rows());
+
+	std::vector<Eigen::Triplet<double> > IJV;
+	IJV.clear();
+
+	/* Implement your code here. */
+	
+	// Set From Triplets Sums all Triplets with the same indices
+	L.setFromTriplets(IJV.begin(), IJV.end());
+}
